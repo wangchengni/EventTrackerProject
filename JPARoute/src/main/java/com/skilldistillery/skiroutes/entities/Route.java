01b54b2997cc06b1e4966f96f7480498cf334ef4
wangchengni <wangchengni001@gmail.com>
2022-01-07 15:34:16 -0700

    Route entity

---
+++ JPARoute/src/main/java/com/skilldistillery/skiroutes/entities/Route.java
@@ -0,0 +1,53 @@
+package com.skilldistillery.skiroutes.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Route {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	public Route() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Route other = (Route) obj;
+		return id == other.id;
+	}
+	@Override
+	public String toString() {
+		return "Route [id=" + id + ", name=" + name + "]";
+	}
+	
+}
---
+++ JPARoute/src/test/java/com/skilldistillery/skiroutes/entities/RouteTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.skiroutes.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RouteTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Route route;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPARoute");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em=emf.createEntityManager();
+		route =em.find(Route.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		route = null;
+		
+	}
+
+	@Test
+	void test() {
+		assertNotNull(route);
+		assertEquals("Last Hoot", route.getName());
+	}
+
+}
