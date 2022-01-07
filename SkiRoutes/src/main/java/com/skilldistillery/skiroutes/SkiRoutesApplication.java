07a15feae3c9b318b86e54004b3d383878c803c7
wangchengni <wangchengni001@gmail.com>
2022-01-07 15:40:52 -0700

    Created Spring Boot

---
+++ SkiRoutes/src/main/java/com/skilldistillery/skiroutes/SkiRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiRoutesApplication.class, args);
+	}
+
+}
